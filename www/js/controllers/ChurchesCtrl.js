18cf090afbd8192dea0ba2f28170a1cbf2b90c89
Wyss Star <surfyst@gmail.com>
2016-02-10 17:43:44 -0500

    alternative approach  for hiding keyboard

--- www/js/controllers/ChurchesCtrl.js
+++ www/js/controllers/ChurchesCtrl.js
@@ -314,7 +314,9 @@ angular.module('starter.controllers')
       if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.close();
       }
+    }
 
+    $scope.onSubmitChurches = function(e) {
     }
 
     $scope.hasValue = hasValue;
